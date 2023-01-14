8aa5aa716c3d3a49e1a45ba543e4ccf8e67ced15
TimeBather <open-source@timebather.cn>
2023-01-14 21:52:12 +0800

    Chore : format the code

--- src/service.ts
+++ src/service.ts
@@ -7,90 +7,86 @@ declare module "koishi" {
   interface Context {
     currency: CurrencyService
   }
+
   interface Tables {
     transaction: Transaction
   }
 }
 
-export interface CurrencyMetaInformation{
-  name? : string
-  unit? : string
-  hidden? : boolean
+export interface CurrencyMetaInformation {
+  name?: string
+  unit?: string
+  hidden?: boolean
 }
 
-export class CurrencyService extends Service{
+export class CurrencyService extends Service {
   currencies: Map<string, BaseCurrencyManager> = new Map();
-  metadata: Map<string,CurrencyMetaInformation> = new Map();
-  default_currency = new BaseCurrencyManager(this.ctx.database,"default::this-do-not-exists")
-  constructor(ctx:Context) {
-    super(ctx,'currency')
+  metadata: Map<string, CurrencyMetaInformation> = new Map();
+  default_currency = new BaseCurrencyManager(this.ctx.database, "default::this-do-not-exists")
+
+  constructor(ctx: Context) {
+    super(ctx, 'currency')
   }
 
   public readonly using = ['database']
 
-  start(){
+  start() {
     injectSession()
     addCommandListeners(this.ctx);
   }
 
-  stop(){
+  stop() {
     revertSession()
   }
 
-  protected __get_base(currencyId:string){
-    if(!this.currencies.has(currencyId))
-      this.currencies.set(currencyId,new BaseCurrencyManager(this.ctx.database,currencyId));
+  protected __get_base(currencyId: string) {
+    if (!this.currencies.has(currencyId))
+      this.currencies.set(currencyId, new BaseCurrencyManager(this.ctx.database, currencyId));
     return this.currencies.get(currencyId);
   }
 
-  async add(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.add>)
-  {
+  async add(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.add>) {
     return await this.__get_base(currencyId).add(...args)
   }
 
-  async cost(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.cost>)
-  {
+  async cost(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.cost>) {
     return await this.__get_base(currencyId).cost(...args)
   }
 
-  async deposit(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.deposit>)
-  {
+  async deposit(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.deposit>) {
     return await this.__get_base(currencyId).deposit(...args)
   }
 
-  async revert(...args : Parameters<typeof BaseCurrencyManager.prototype.revert>)
-  {
+  async revert(...args: Parameters<typeof BaseCurrencyManager.prototype.revert>) {
     return await this.default_currency.revert(...args)
   }
 
-  async get(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.get>)
-  {
+  async get(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.get>) {
     return await this.__get_base(currencyId).get(...args)
   }
 
-  async set(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.set>)
-  {
+  async set(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.set>) {
     return await this.__get_base(currencyId).set(...args)
   }
 
-  async confirm(...args : Parameters<typeof BaseCurrencyManager.prototype.confirm>){
+  async confirm(...args: Parameters<typeof BaseCurrencyManager.prototype.confirm>) {
     return await this.default_currency.confirm(...args)
   }
 
-  async transfer(currencyId:string,...args : Parameters<typeof BaseCurrencyManager.prototype.transfer>){
+  async transfer(currencyId: string, ...args: Parameters<typeof BaseCurrencyManager.prototype.transfer>) {
     return await this.__get_base(currencyId).transfer(...args)
   }
 
-  extends(currencyId:keyof Currencies,meta?:CurrencyMetaInformation){
+  extends(currencyId: keyof Currencies, meta?: CurrencyMetaInformation) {
     this.ctx.database.extend('user', {
         [currencyId]: {
-          type:'double',
-          initial:0
+          type: 'double',
+          initial: 0
         },
       }
     )
-    if(meta && !this.metadata.has(currencyId)){
-      this.metadata.set(currencyId,meta);
+    if (meta && !this.metadata.has(currencyId)) {
+      this.metadata.set(currencyId, meta);
     }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,19 @@
 import {Transaction} from "./base";
 
-export type EconomicSingleEntityOperation = (currencyId:string, amount:number, reason:string)=>Transaction
+export type EconomicSingleEntityOperation = (currencyId: string, amount: number, reason: string) => Transaction
 
-export type EconomicMultiEntityOperation = (currencyId:string, toUserId:number, amount:number, reason:string)=>Transaction
+export type EconomicMultiEntityOperation = (currencyId: string, toUserId: number, amount: number, reason: string) => Transaction
 
-export type EconomicTransactionOperation = (transaction:Transaction)=>void
+export type EconomicTransactionOperation = (transaction: Transaction) => void
 
-export abstract class EconomicError extends Error{
+export abstract class EconomicError extends Error {
   readonly abstract error_type
 }
 
-export class InsufficientBalanceError extends EconomicError{
+export class InsufficientBalanceError extends EconomicError {
   readonly error_type = 'insufficient_balance'
-    constructor(){
-      super('Insufficient balance')
-    }
+
+  constructor() {
+    super('Insufficient balance')
+  }
 }
